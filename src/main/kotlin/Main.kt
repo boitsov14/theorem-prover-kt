ab90c628ef08fe868941d34d7918bfb628bd801d
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2021-09-07 23:14:06 +0900

    全面書き直し
    インターフェースや継承をフルに生かした形で全面的に書き直した．

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,180 @@
 fun main() {
+    val fml1 = Atom("P")
+    val fml11 = Atom("P")
+    println(fml1 == fml11)
+    println(fml1)
+    println(UnaryConnectiveType.NOT)
+    val fml2 = UnaryConnective(UnaryConnectiveType.NOT, fml1)
+    val fml21 = UnaryConnective(UnaryConnectiveType.NOT, fml11)
+    println(fml2 == fml21)
+    println(fml2)
+    val fml3 = BinaryConnective(BinaryConnectiveType.AND, fml1, fml2)
+    println(fml3)
+    println(BinaryConnectiveType.values().map{it.chr})
+    val goal1 = Goal(mutableListOf(fml3,fml2), fml1)
+    println(goal1)
+}
+
+interface Formula {
+    override fun toString(): String
+}
+
+data class Atom(val name: String): Formula {
+    override fun toString(): String = name
+}
+
+val falseFormula = Atom("false")
+
+interface ConnectiveType {
+    val chr: Char
+    val precedence: Int
+    override fun toString(): String
+}
+
+interface Connective: Formula {
+    val connective: ConnectiveType
+}
+
+enum class UnaryConnectiveType(override val chr: Char, override val precedence: Int): ConnectiveType {
+    NOT('¬',4);
+    override fun toString() = "$chr"
+}
+
+data class UnaryConnective(override val connective: UnaryConnectiveType, val formula: Formula): Connective {
+    override fun toString(): String = "($connective$formula)"
+}
+
+enum class BinaryConnectiveType(override val chr: Char, override val precedence: Int): ConnectiveType {
+    IMPLY('→', 1),
+    AND('∧', 3),
+    OR('∨', 2),
+    IFF('↔', 0);
+    override fun toString(): String = "$chr"
+}
+
+data class BinaryConnective(override val connective: BinaryConnectiveType, val left: Formula, val right: Formula): Connective {
+    override fun toString(): String = "($left $connective $right)"
+}
+
+data class Goal(var assumptions: MutableList<Formula>, var conclusion: Formula) {
+    constructor (conclusion: Formula) : this(mutableListOf(), conclusion)
+    override fun toString(): String {
+        var str = ""
+        assumptions.forEach { str += "$it".removeSurrounding("(", ")") + ", "}
+        str = str.removeSuffix(", ")
+        if (str.isNotEmpty()) { str += " " }
+        return "$str⊢ $conclusion"
+    }
+}
+
+typealias Goals = MutableList<Goal>
+
+interface TacticType {
+    val id: String
+    override fun toString(): String
+    fun canApply(goal: Goal): Boolean
+    fun apply(goals: Goals)
+}
+
+interface TacticType0: TacticType {}
+
+enum class Tactic0(override val id: String): TacticType0 {
+    ASSUMPTION("assumption"),
+    INTRO("intro"),
+    SPLIT("split"),
+    LEFT("left"),
+    RIGHT("right"),
+    EXFALSO("exfalso"),
+    BY_CONTRA("by_contra");
+    override fun toString(): String = name
+    override fun canApply(goal: Goal): Boolean = when(this) {
+        ASSUMPTION			-> goal.conclusion in goal.assumptions
+        INTRO				-> (goal.conclusion as? Connective)?.connective in setOf(BinaryConnectiveType.IMPLY, UnaryConnectiveType.NOT)
+        SPLIT				-> (goal.conclusion as? Connective)?.connective in setOf(BinaryConnectiveType.AND, BinaryConnectiveType.IFF)
+        LEFT, RIGHT			-> (goal.conclusion as? Connective)?.connective == BinaryConnectiveType.OR
+        EXFALSO, BY_CONTRA	-> goal.conclusion != falseFormula
+    }
+    override fun apply(goals: Goals) {
+        val goal = goals[0]
+        when(this) {
+            ASSUMPTION -> goals.removeAt(0)
+            INTRO -> when((goal.conclusion as Connective).connective) {
+                BinaryConnectiveType.IMPLY  -> {
+                    goal.assumptions.add((goal.conclusion as BinaryConnective).left)
+                    goal.conclusion = (goal.conclusion as BinaryConnective).right
+                }
+                UnaryConnectiveType.NOT -> {
+                    goal.assumptions.add((goal.conclusion as UnaryConnective).formula)
+                    goal.conclusion = falseFormula
+                }
+            }
+            SPLIT -> when((goal.conclusion as Connective).connective) {
+                BinaryConnectiveType.AND    -> {
+                    val left = Goal(goal.assumptions.toMutableList(), (goal.conclusion as BinaryConnective).left)
+                    val right = Goal(goal.assumptions.toMutableList(), (goal.conclusion as BinaryConnective).right)
+                    goals.removeAt(0)
+                    goals.add(0, left)
+                    goals.add(1, right)
+                }
+                BinaryConnectiveType.IFF    -> {
+                    val toLeft = Goal(goal.assumptions.toMutableList(), BinaryConnective(BinaryConnectiveType.IMPLY, (goal.conclusion as BinaryConnective).left, (goal.conclusion as BinaryConnective).right))
+                    val toRight = Goal(goal.assumptions.toMutableList(), BinaryConnective(BinaryConnectiveType.IMPLY, (goal.conclusion as BinaryConnective).right, (goal.conclusion as BinaryConnective).left))
+                    goals.removeAt(0)
+                    goals.add(0, toLeft)
+                    goals.add(1, toRight)
+                }
+            }
+            LEFT -> goal.conclusion = (goal.conclusion as BinaryConnective).left
+            RIGHT -> goal.conclusion = (goal.conclusion as BinaryConnective).right
+            EXFALSO -> goal.conclusion = falseFormula
+            BY_CONTRA -> {
+                goal.assumptions.add(UnaryConnective(UnaryConnectiveType.NOT, goal.conclusion))
+                goal.conclusion = falseFormula
+            }
+        }
+    }
+}
+
+
+
+
+/*
+
+fun isConnective(chr: Char): Boolean = chr in Connective.values().map{it.chr}
+
+fun getConnective(chr: Char) : Connective? = Connective.values().find{it.chr == chr}
+
+enum class Tactic(val ID: String, val arity: Int){
+    APPLY("apply", 1),
+    CASES("cases", 1),
+    HAVE("have", 2),
+    CLEAR("clear", 1);
+    fun canApply(goal: Goal): Boolean = when(this) {
+        CLEAR				-> goal.assumptions.isNotEmpty() //もしくはavailablesで
+        else                -> possibleAssumptions(goal).isNotEmpty()
+    }
+    fun possibleAssumptions(goal: Goal): List<Formula> = when(this) {
+        APPLY   -> goal.assumptions
+            .filter { (it.connective == Connective.IMPLY && it.right == goal.conclusion)
+                    || (it.connective == Connective.NOT && goal.conclusion == Formula('⊥')) }
+        CASES   -> goal.assumptions.filter { it.connective in listOf(Connective.AND, Connective.OR, Connective.IFF) }
+        else    -> listOf()
+    }
+    fun apply(goals: Goals) {
+        val goal = goals[0]
+        when(this) {
+            APPLY       -> TODO()
+            CASES       -> TODO()
+            HAVE        -> TODO()
+            CLEAR       -> TODO()
+        }
+    }
+}
+*/
+
+/*
+
+fun main() {
     val formula0 = Formula(Formula('P'), Formula('Q'), Connective.IMPLY)
     val formula1 = Formula(Formula('P'), Formula('Q'), Connective.IMPLY)
     println(formula0 == formula1)
@@ -29,117 +205,4 @@ fun main() {
     println(str.removeSurrounding("(", ")"))
 }
 
-data class Formula(val left: Formula?, val right: Formula?, val connective: Connective?, val atom: Char? = null){
-    constructor (atom: Char?) : this(null, null, null, atom)
-    private fun toString0(): String {
-        if (atom == '⊥')
-            return "false"
-        if (atom != null)
-            return "$atom"
-        if (left != null && connective == Connective.NOT)
-            return "${Connective.NOT.chr}${left.toString0()}"
-        if (left != null && right != null && connective != null)
-            return "(${left.toString0()} ${connective.chr} ${right.toString0()})"
-        return "Something is wrong." //エラー処理
-    }
-    override fun toString(): String = this.toString0().removeSurrounding("(", ")")
-}
-
-data class Goal(var assumptions: MutableList<Formula>, var conclusion: Formula) {
-    constructor (conclusion: Formula) : this(mutableListOf(), conclusion)
-    override fun toString(): String {
-        var str = ""
-        assumptions.forEach { str += "$it, " }
-        str = str.removeSuffix(", ")
-        if (str.isNotEmpty()) { str += " " }
-        return "$str|- $conclusion"
-    }
-} //コンストラクタを別に定義した方がいいかも．リストをセットしたらリストのコピーを格納してほしい．
-
-typealias Goals = MutableList<Goal>
-
-enum class Connective(val chr: Char, val precedence: Int){
-    IMPLY('→', 1),
-    AND('∧', 3),
-    OR('∨', 2),
-    IFF('↔', 0),
-    NOT('¬', 4);
-    override fun toString(): String = "$chr"
-}
-
-fun isConnective(chr: Char): Boolean = chr in Connective.values().map{it.chr}
-
-fun getConnective(chr: Char) : Connective? = Connective.values().find{it.chr == chr}
-
-enum class Tactic(val ID: String, val arity: Int){
-    ASSUMPTION("assumption", 0),
-    INTRO("intro", 0),
-    SPLIT("split", 0),
-    APPLY("apply", 1),
-    CASES("cases", 1),
-    HAVE("have", 2),
-    LEFT("left", 0),
-    RIGHT("right", 0),
-    EXFALSO("exfalso", 0),
-    BY_CONTRA("by_contra", 0),
-    CLEAR("clear", 1);
-    fun canApply(goal: Goal): Boolean = when(this) {
-        ASSUMPTION			-> goal.conclusion in goal.assumptions
-        INTRO				-> goal.conclusion.connective in listOf(Connective.IMPLY, Connective.NOT)
-        SPLIT				-> goal.conclusion.connective in listOf(Connective.AND, Connective.IFF)
-        LEFT, RIGHT			-> goal.conclusion.connective == Connective.OR
-        EXFALSO, BY_CONTRA	-> goal.conclusion.atom != '⊥'
-        CLEAR				-> goal.assumptions.isNotEmpty() //もしくはavailablesで
-        else                -> possibleAssumptions(goal).isNotEmpty()
-    }
-    fun possibleAssumptions(goal: Goal): List<Formula> = when(this) {
-        APPLY   -> goal.assumptions
-            .filter { (it.connective == Connective.IMPLY && it.right == goal.conclusion)
-                    || (it.connective == Connective.NOT && goal.conclusion == Formula('⊥')) }
-        CASES   -> goal.assumptions.filter { it.connective in listOf(Connective.AND, Connective.OR, Connective.IFF) }
-        else    -> listOf()
-    }
-    fun apply(goals: Goals) {
-        val goal = goals[0]
-        when(this) {
-            ASSUMPTION  -> goals.removeAt(0)
-            INTRO       -> when(goal.conclusion.connective) {
-                Connective.IMPLY    -> {
-                    goal.assumptions.add(goal.conclusion.left!!)
-                    goal.conclusion = goal.conclusion.right!!
-                }
-                Connective.NOT      -> {
-                    goal.assumptions.add(goal.conclusion.left!!)
-                    goal.conclusion = Formula('⊥')
-                }
-            }
-            SPLIT       -> when(goal.conclusion.connective) {
-                    Connective.AND  -> {
-                        val left = Goal(goal.assumptions.toMutableList(), goal.conclusion.left!!)
-                        val right = Goal(goal.assumptions.toMutableList(), goal.conclusion.right!!)
-                        goals.removeAt(0)
-                        goals.add(0, left)
-                        goals.add(1, right)
-                    }
-                    Connective.IFF  -> {
-                        val toLeft = Goal(goal.assumptions.toMutableList(), Formula(goal.conclusion.left, goal.conclusion.right, Connective.IMPLY))
-                        val toRight = Goal(goal.assumptions.toMutableList(), Formula(goal.conclusion.right, goal.conclusion.left, Connective.IMPLY))
-                        goals.removeAt(0)
-                        goals.add(0, toLeft)
-                        goals.add(1, toRight)
-                    }
-                }
-            APPLY       -> TODO()
-            CASES       -> TODO()
-            HAVE        -> TODO()
-            LEFT        -> goal.conclusion = goal.conclusion.left!!
-            RIGHT       -> goal.conclusion = goal.conclusion.right!!
-            EXFALSO     -> goal.conclusion = Formula('⊥')
-            BY_CONTRA   -> {
-                goal.assumptions.add(Formula(goal.conclusion, null, Connective.NOT))
-                goal.conclusion = Formula('⊥')
-            }
-            CLEAR       -> TODO()
-        }
-    }
-}
+ */
