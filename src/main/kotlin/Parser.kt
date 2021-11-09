ddb1b1bc2972a2372f1deca0694c40cd9815e79f
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2021-11-10 03:55:56 +0900

    Parserの修正

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -183,11 +183,9 @@ fun preTokenize(inputChrs: ArrayDeque<Char>): ArrayDeque<PreToken> {
 					chrs.add(inputChrs.removeFirst())
 				}
 				val str = String(chrs.toCharArray())
-				if (currentChr.isUpperCase()) {
-					preTokens.add(PreToken.PREDICATE(str))
-				}
-				if (currentChr.isLowerCase()) {
-					preTokens.add(PreToken.VAR(str))
+				when(currentChr) {
+					in ('A'..'Z') -> preTokens.add(PreToken.PREDICATE(str))
+					in ('a'..'z') -> preTokens.add(PreToken.VAR(str))
 				}
 			}
 			else -> throw FormulaParserException("Illegal Argument >> $currentChr")
@@ -200,8 +198,8 @@ fun tokenize(preTokens: ArrayDeque<PreToken>): ArrayDeque<Token> {
 	val tokens = ArrayDeque<Token>()
 	while (preTokens.isNotEmpty()) {
 		when (val preToken = preTokens.removeFirst()) {
-			PreToken.Symbol.LEFT_PARENTHESIS 	-> tokens.add(Token.Symbol.LEFT_PARENTHESIS)
-			PreToken.Symbol.RIGHT_PARENTHESIS 	-> tokens.add(Token.Symbol.RIGHT_PARENTHESIS)
+			PreToken.Symbol.LEFT_PARENTHESIS 	-> tokens.add(Token.LEFT_PARENTHESIS)
+			PreToken.Symbol.RIGHT_PARENTHESIS 	-> tokens.add(Token.RIGHT_PARENTHESIS)
 			PreToken.Symbol.NOT 				-> tokens.add(Token.Operator.Unary.NOT)
 			PreToken.Symbol.AND 				-> tokens.add(Token.Operator.Binary.AND)
 			PreToken.Symbol.OR 					-> tokens.add(Token.Operator.Binary.OR)
@@ -218,14 +216,11 @@ fun tokenize(preTokens: ArrayDeque<PreToken>): ArrayDeque<Token> {
 				tokens.add(predicate)
 			}
 			PreToken.Symbol.ALL, PreToken.Symbol.EXISTS -> {
-				if (!(preTokens.isNotEmpty() && preTokens.first() is PreToken.VAR)) {
+				if (!(preTokens.size >= 2 && preTokens[0] is PreToken.VAR && preTokens[1] == PreToken.Symbol.COMMA)) {
 					throw FormulaParserException("Parse Error with quantifier")
 				}
 				val preTokenVar = preTokens.removeFirst() as PreToken.VAR
 				val bddVar = Var(preTokenVar.id)
-				if (!(preTokens.isNotEmpty() && preTokens.first() == PreToken.Symbol.COMMA)) {
-					throw FormulaParserException("Parse Error with quantifier")
-				}
 				preTokens.removeFirst()
 				when(preToken) {
 					PreToken.Symbol.ALL 	-> tokens.add(Token.Operator.Unary.ALL(bddVar))
@@ -245,9 +240,9 @@ fun toReversePolishNotation(inputTokens: ArrayDeque<Token>): ArrayDeque<Token> {
 		when(token) {
 			Token.FALSE -> outputTokens.add(token)
 			is Token.PREDICATE -> outputTokens.add(token)
-			Token.Symbol.LEFT_PARENTHESIS -> stack.addFirst(token)
-			Token.Symbol.RIGHT_PARENTHESIS -> {
-				while (stack.isNotEmpty() && stack.first() != Token.Symbol.LEFT_PARENTHESIS) {
+			Token.LEFT_PARENTHESIS -> stack.addFirst(token)
+			Token.RIGHT_PARENTHESIS -> {
+				while (stack.isNotEmpty() && stack.first() != Token.LEFT_PARENTHESIS) {
 					outputTokens.add(stack.removeFirst())
 				}
 				if (stack.isEmpty()) {
@@ -266,7 +261,7 @@ fun toReversePolishNotation(inputTokens: ArrayDeque<Token>): ArrayDeque<Token> {
 			}
 		}
 	}
-	if (Token.Symbol.LEFT_PARENTHESIS in stack) {
+	if (Token.LEFT_PARENTHESIS in stack) {
 		throw FormulaParserException("Parenthesis Error")
 	}
 	outputTokens.addAll(stack)
@@ -277,25 +272,25 @@ fun toFormula(tokens: ArrayDeque<Token>): Formula {
 	val stack = ArrayDeque<Formula>()
 	for (token in tokens) {
 		when(token) {
-			Token.FALSE -> stack.add(Formula.FALSE)
-			is Token.PREDICATE -> stack.add(Formula.PREDICATE(token.id, token.vars))
+			Token.FALSE -> stack.addFirst(Formula.FALSE)
+			is Token.PREDICATE -> stack.addFirst(Formula.PREDICATE(token.id, token.vars))
 			is Token.Operator.Unary -> {
 				if (stack.isEmpty()) {
 					throw FormulaParserException("Parse Error")
 				}
-				val fml = stack.removeLast()
+				val fml = stack.removeFirst()
 				when(token) {
-					Token.Operator.Unary.NOT -> stack.add(Formula.NOT(fml))
+					Token.Operator.Unary.NOT -> stack.addFirst(Formula.NOT(fml))
 					is Token.Operator.Unary.ALL -> {
 						try {
-							stack.add(Formula.ALL(token.bddVar, fml))
+							stack.addFirst(Formula.ALL(token.bddVar, fml))
 						} catch (e: DuplicateBddVarException) {
 							throw FormulaParserException("bounded variable is duplicated.")
 						}
 					}
 					is Token.Operator.Unary.EXISTS -> {
 						try {
-							stack.add(Formula.EXISTS(token.bddVar, fml))
+							stack.addFirst(Formula.EXISTS(token.bddVar, fml))
 						} catch (e: DuplicateBddVarException) {
 							throw FormulaParserException("bounded variable is duplicated.")
 						}
@@ -306,13 +301,13 @@ fun toFormula(tokens: ArrayDeque<Token>): Formula {
 				if (stack.size < 2) {
 					throw FormulaParserException("Parse Error")
 				}
-				val rightFml = stack.removeLast()
-				val leftFml = stack.removeLast()
+				val rightFml = stack.removeFirst()
+				val leftFml = stack.removeFirst()
 				when(token) {
-					Token.Operator.Binary.AND -> stack.add(Formula.AND(leftFml, rightFml))
-					Token.Operator.Binary.OR -> stack.add(Formula.OR(leftFml, rightFml))
-					Token.Operator.Binary.IMPLIES -> stack.add(Formula.IMPLIES(leftFml, rightFml))
-					Token.Operator.Binary.IFF -> stack.add(Formula.IFF(leftFml, rightFml))
+					Token.Operator.Binary.AND -> 		stack.addFirst(Formula.AND(leftFml, rightFml))
+					Token.Operator.Binary.OR -> 		stack.addFirst(Formula.OR(leftFml, rightFml))
+					Token.Operator.Binary.IMPLIES -> 	stack.addFirst(Formula.IMPLIES(leftFml, rightFml))
+					Token.Operator.Binary.IFF -> 		stack.addFirst(Formula.IFF(leftFml, rightFml))
 				}
 			}
 			else -> throw IllegalArgumentException()
@@ -343,28 +338,20 @@ sealed interface PreToken {
 }
 
 sealed interface Token {
-	enum class Symbol: Token {
-		LEFT_PARENTHESIS,
-		RIGHT_PARENTHESIS,
-	}
+	object LEFT_PARENTHESIS: Token
+	object RIGHT_PARENTHESIS: Token
 	sealed interface Operator: Token {
 		val precedence: Int
-		enum class Binary: Operator {
-			AND 	{ override val precedence = 4 },
-			OR		{ override val precedence = 3 },
-			IMPLIES	{ override val precedence = 2 },
-			IFF 	{ override val precedence = 1 }
+		enum class Binary(override val precedence: Int): Operator {
+			AND(4),
+			OR(3),
+			IMPLIES(2),
+			IFF(1)
 		}
 		sealed interface Unary: Operator {
-			object NOT: Unary {
-				override val precedence = 5
-			}
-			data class ALL(val bddVar: Var): Unary {
-				override val precedence = 0
-			}
-			data class EXISTS(val bddVar: Var): Unary {
-				override val precedence = 0
-			}
+			object NOT: Unary {	override val precedence = 5 }
+			data class ALL(val bddVar: Var): Unary { override val precedence = 0 }
+			data class EXISTS(val bddVar: Var): Unary { override val precedence = 0 }
 		}
 	}
 	data class PREDICATE(val id: String, val vars: List<Var>): Token
