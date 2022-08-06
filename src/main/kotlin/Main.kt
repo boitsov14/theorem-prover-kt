fd17a4b42ade26d7fabbd2d127059c0b46d3cd7d
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2022-08-06 22:21:55 +0900

    mainForJarの修正

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,15 +13,14 @@ suspend fun main(args: Array<String>) {
 suspend fun mainForJar(args: Array<String>) {
 	val id = args[0]
 	val sequentStr = args[1]
-	val messageFile = File("${id}_message.txt")
 	// Parser
 	val sequent = try {
 		sequentStr.parseToSequent()
 	} catch (e: FormulaParserException) {
-		messageFile.writeText(e.message!!)
+		print(e.message)
 		return
 	} catch (e: Throwable) {
-		messageFile.writeText("An unexpected error has occurred: $e")
+		print("An unexpected error has occurred: $e")
 		return
 	}
 	// Prover
@@ -31,28 +30,27 @@ suspend fun mainForJar(args: Array<String>) {
 			rootNode.prove()
 		}
 	} catch (e: TimeoutCancellationException) {
-		messageFile.writeText("Proof Failed: Timeout.")
+		print("Proof Failed: Timeout.")
 		return
 	} catch (e: OutOfMemoryError) {
-		messageFile.writeText("Proof Failed: OutOfMemoryError.")
+		print("Proof Failed: OutOfMemoryError.")
 		return
 	} catch (e: Throwable) {
-		messageFile.writeText("An unexpected error has occurred: $e")
+		print("An unexpected error has occurred: $e")
 		return
 	}
-	messageFile.writeText(proofState.toString())
+	print(proofState)
 	// TeX
 	val latexOutput = try {
 		rootNode.getLatexOutput(proofState)
 	} catch (e: OutOfMemoryError) {
-		messageFile.appendText("The proof tree is too large to output: OutOfMemoryError.")
+		print("The proof tree is too large to output: OutOfMemoryError.")
 		return
 	} catch (e: Throwable) {
-		messageFile.appendText("An unexpected error has occurred: $e")
+		print("An unexpected error has occurred: $e")
 		return
 	}
-	val latexFile = File("${id}.tex")
-	latexFile.writeText(latexOutput)
+	File("${id}.tex").writeText(latexOutput)
 }
 
 suspend fun mainForConsole() {
