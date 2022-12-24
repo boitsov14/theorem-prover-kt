acefd75267a4b766a391ca5fd188fd27c2e3ca5f
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2022-12-24 23:04:56 +0900

    refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,43 @@
 import core.FormulaParserException
 import sequentProver.*
+import sequentProver.ProofState.*
 import java.io.File
 
 suspend fun main(args: Array<String>) {
-	try {
-		mainForJar(args)
-	} catch (e: Exception) {
-		print("An unexpected error has occurred: $e")
+	if (args.isNotEmpty()) {
+		prover(args[0], args[1])
+	} else {
+		prover()
 	}
-	//temp()
-	//mainForConsole()
 }
 
-suspend fun mainForJar(args: Array<String>) {
-	val id = args[0]
-	val sequentStr = args[1]
-	// Parser
+suspend fun prover(id: String, sequentString: String) {
+	// Parse
 	val sequent = try {
-		sequentStr.parseToSequent()
+		sequentString.parseToSequent()
 	} catch (e: FormulaParserException) {
 		print(e.message)
 		return
 	}
-	// Prover
-	val rootNode = Node(sequent)
-	val proofState = rootNode.prove()
+	// Prove
+	val start = System.currentTimeMillis()
+	val (proofState, node) = sequent.prove()
+	val end = System.currentTimeMillis()
+	print(proofState)
+	if (proofState == Provable || proofState == Unprovable) {
+		print(" Completed in ${(end - start) / 1000.0} seconds.")
+	}
 	// TeX
-	File("${id}.tex").writeText(rootNode.getLatexOutput(proofState))
+	File("${id}.tex").writeText(node.getLatex(proofState))
+}
+
+suspend fun prover() {
+	print("INPUT A FORMULA >>> ")
+	val sequent = readln().parseToSequent()
+	val start = System.currentTimeMillis()
+	val (proofState, node) = sequent.prove()
+	val end = System.currentTimeMillis()
+	println(proofState)
+	println("Completed in ${end - start} ms")
+	File("src/main/resources/Output.tex").writeText(node.getLatex(proofState))
 }
