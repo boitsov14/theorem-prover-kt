1eccbe9a4a008b46991642bee42c920643ba0e9a
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-11-14 21:44:34 +0900

    wip

--- src/main/kotlin/sequentProver/Parser.kt
+++ src/main/kotlin/sequentProver/Parser.kt
@@ -1,32 +1,37 @@
 package sequentProver
 
 import core.*
+import java.text.Normalizer
 
 private fun String.toFormulas(): Set<Formula> {
 	val fmls = mutableSetOf<Formula>()
 	var counter = 0
 	var startPos = 0
-	for ((index, chr) in this.withIndex()) {
+	for ((index, chr) in withIndex()) {
 		when (chr) {
 			'(' -> counter++
 			')' -> counter--
 			',' -> {
 				if (counter == 0) {
-					val fml = this.substring(startPos, index).parseToFormula()
-					fmls.add(fml)
-					startPos = index + 1
+					val str = substring(startPos, index)
+					if (('∀' !in str && '∃' !in str) || !("""[a-zA-Zα-ωΑ-Ω]\d*(,[a-zA-Zα-ωΑ-Ω]\d*)*""".toRegex()
+							.matches(str.takeLastWhile { it !in setOf('∀', '∃') }))
+					) {
+						fmls.add(str.parseToFormula())
+						startPos = index + 1
+					}
 				}
 			}
 		}
 	}
 	if (counter != 0) throw FormulaParserException("Parenthesis Error.")
-	val lastFml = this.substring(startPos).parseToFormula()
+	val lastFml = substring(startPos).parseToFormula()
 	fmls.add(lastFml)
 	return fmls
 }
 
 fun String.parseToSequent(): Sequent {
-	val str = this.toOneLetter()
+	val str = Normalizer.normalize(this, Normalizer.Form.NFKC).toOneLetter().trimSpace()
 	return when (str.count { it == '⊢' }) {
 		0 -> {
 			val fml = str.parseToFormula()
@@ -35,12 +40,12 @@ fun String.parseToSequent(): Sequent {
 
 		1 -> {
 			val strList = str.split("⊢")
-			val assumptions = if (strList[0].trim().isNotEmpty()) {
+			val assumptions = if (strList[0].isNotEmpty()) {
 				strList[0].toFormulas()
 			} else {
 				emptySet()
 			}
-			val conclusions = if (strList[1].trim().isNotEmpty()) {
+			val conclusions = if (strList[1].isNotEmpty()) {
 				strList[1].toFormulas()
 			} else {
 				emptySet()
