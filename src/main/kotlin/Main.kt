5d842df384d942dad5ee0c3807206acceb58e4e0
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-11-14 21:52:19 +0900

    wip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,19 @@
+import core.Formula
 import core.FormulaParserException
-import sequentProver.*
-import sequentProver.ProofState.*
+import core.parseToFormula
+import sequentProver.ProofState.Provable
+import sequentProver.ProofState.Unprovable
+import sequentProver.Sequent
+import sequentProver.getLatex
+import sequentProver.parseToSequent
+import sequentProver.prove
 import java.io.File
 
 suspend fun main(args: Array<String>) {
 	if (args.isNotEmpty()) {
 		prover(args[0], args[1])
 	} else {
+		//example()
 		prover()
 	}
 }
@@ -28,16 +35,43 @@ suspend fun prover(id: String, sequentString: String) {
 		print(" Completed in ${(end - start) / 1000.0} seconds.")
 	}
 	// TeX
-	File("${id}.tex").writeText(node.getLatex(proofState))
+	File("${id}.tex").writeText(node.getLatex())
 }
 
 suspend fun prover() {
 	print("INPUT A FORMULA >>> ")
 	val sequent = readln().parseToSequent()
+	println(sequent)
 	val start = System.currentTimeMillis()
 	val (proofState, node) = sequent.prove()
 	val end = System.currentTimeMillis()
 	println(proofState)
 	println("Completed in ${end - start} ms")
-	File("src/main/resources/Output.tex").writeText(node.getLatex(proofState))
+	// File("src/main/resources/Output.tex").writeText(node.getLatex2())
+	// listOf(node).printProof()
+}
+
+suspend fun example() {
+	val lines = File("src/main/resources/Examples.txt").readLines().iterator()
+	var time = 0L
+	while (lines.hasNext()) {
+		print(lines.next())
+		print(": ")
+		val fmls = mutableListOf<Formula>()
+		while (lines.hasNext()) {
+			val line = lines.next()
+			if (line.isEmpty()) break
+			fmls += line.parseToFormula()
+		}
+		val assumptions = fmls.dropLast(1).toSet()
+		val conclusions = setOf(fmls.last())
+		val sequent = Sequent(assumptions, conclusions)
+		val start = System.currentTimeMillis()
+		val (proofState, _) = sequent.prove()
+		val end = System.currentTimeMillis()
+		println(proofState)
+		time += end - start
+		println(end - start)
+	}
+	println("Completed in $time ms")
 }
