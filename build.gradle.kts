d66b9d12571f594ce17eed574cb07625f2c575e3
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-11-14 21:16:12 +0900

    gradle init

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
